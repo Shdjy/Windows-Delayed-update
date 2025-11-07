79896c99f46c168ff709368cf095d63d420574ad
Sunhd <131387730+Shdjy@users.noreply.github.com>
2025-11-07 09:30:44 +0800

    Add files via upload

--- Delayed-update.bat
+++ Delayed-update.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 :: 以管理员身份运行
 net session >nul 2>&1
 if %errorLevel% neq 0 (
@@ -10,5 +11,5 @@ if %errorLevel% neq 0 (
 :: 修改注册表
 reg add "HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" /v FlightSettingsMaxPauseDays /t REG_DWORD /d 3650 /f
 
-echo The number of days for suspension of updates has been successfully set to 3650 days.
+echo 已成功将暂停更新天数设置为 3650 天
 pause
